d75d4ee02197767f87d22896f8dc6588a5066777
MeowBa <117983568+MeowBa@users.noreply.github.com>
2022-12-26 14:38:49 +0800

    add php

---
+++ PHP/index.php
@@ -0,0 +1,347 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.php">
+    
+</head>
+<body>
+    <!-- 這裡是header -->
+    <div class="header">
+        <div class="logo-centered">
+            <a href="./home.html" class="active"> <img src="./img/LOGO.png" style="width: 150px; ;"> </a>
+        </div>
+        <button class="openbtn" onclick="openNav()">&#9776; </button>
+
+        <div class="header-right">
+            <a href="./car.html" target="_blank">購物車</a>
+            <a href="./login.html" target="_blank">登入|註冊</a>
+        </div>
+
+    </div>
+        <!-- 這裡是漢堡打開內容 -->
+        <div id="myMenu" class="menu">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="./food.html">貓狗飼料</a>
+        <a href="#">貓狗罐頭</a>
+        <a href="./candy.html">貓狗零食</a>
+        <a href="./health.html">寵物保健</a>
+        <a href="#">貓砂系列</a>
+        <a href="./use.html">居家用品</a>
+        <a href="#">抓板玩具</a>
+        <a href="./pretty.html">美容護理</a>
+        <a href="#">環境清潔</a>
+        <a href="./hotel.html">住宿推薦</a>
+    </div>
+        <!-- 這裡是icon列 -->
+    <div id="icons">
+        <a href="./food.html">
+            <img src="icons/pet-food.png" alt="貓狗飼料">
+            <p>貓狗飼料</p>
+        </a>
+        <a href="./candy.html">
+            <img src="icons/dog-treat (1).png" alt="寵物零食">
+            <p>寵物零食</p>
+        </a>
+        <a href="./use.html">
+            <img src="icons/cat-toy.png" alt="寵物用品">
+            <p>寵物用品</p>
+        </a>
+        <a href="./health.html">
+            <img src="icons/medicine.png" alt="寵物保健">
+            <p>寵物保健</p>
+        </a>
+        <a href="./pretty.html">
+            <img src="icons/pet-shampoo (1).png" alt="寵物美容">
+            <p>寵物美容</p>
+        </a>
+        <a href="./hotel.html">
+            <img src="icons/house.png" alt="寵物住宿">
+            <p>寵物住宿</p>
+        </a>
+    </div>
+    <!-- 這裡是搜尋列 -->
+    <div class="search">
+        <form>
+            <input type="text" placeholder="搜尋..">
+            <button type="submit"><img src="icons/search.png" alt="Search" style="width: 25px;"></button>
+        </form>
+    </div>
+    <!-- 這裡是廣告輪播 -->
+    <div class="slideshow-container">
+        <div class="mySlides fade">
+            <img src="img/adv_1.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <img src="img/adv_3.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <img src="img/adv_10.jpg" style="width:100%">
+        </div>
+        <!-- 下一張及上一張的按鈕 -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        <!-- 廣告圖下面的點點 -->
+        <div class="dots">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+    </div>
+    <br>
+    <!-- 熱銷榜 -->
+    <div class="HotList">
+        <span class="Hot animate__heartBeat">熱銷排行榜</span>
+        <button class="tablink" onclick="openPage('Stay', this, 'var(--maincolor)')">寵物住宿</button>
+        <button class="tablink" onclick="openPage('Pretty', this, 'var(--maincolor)')">美容護理</button>
+        <button class="tablink" onclick="openPage('Health', this, 'var(--maincolor)')">保健食品</button>
+        <button class="tablink" onclick="openPage('Candy', this, 'var(--maincolor)')">寵物零食</button>
+        <button class="tablink" onclick="openPage('Use', this, 'var(--maincolor)')">寵物用品</button>
+        <button class="tablink" onclick="openPage('Food', this, 'var(--maincolor)')" id="defaultOpen">飼料排行</button>
+
+        <!-- 飼料熱銷榜 -->
+        <div id="Food" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/dogfood_3.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"> <br> 零穀 全齡犬敏感配方(羊肉+鮭魚)5.5磅(狗飼料)</a> <br>
+                    <span class="price"> $664</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/catfood_7.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"> <br> 莫比 Mobby 愛貓無穀配方【鱒魚+馬鈴薯】1.5公斤 (貓飼料)</a> <br>
+                    <span class="price"> $621</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/原點_1.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>原點 無穀雞肉 5公斤</a> <br>
+                    <span class="price"> $2,521</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 寵物用品熱銷榜 -->
+        <div id="Use" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/用品_4.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>渦流循環寵物活水機1.5L(綠) W2-N</a> <br>
+                    <span class="price"> $990</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/用品_5.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>甜甜圈六餐餵食器 (粉)(環保版) F6-TW</a> <br>
+                    <span class="price"> $990</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/用品_6.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>高質感可折疊雙肩寵物包【淺灰色】(40* 26* 33cm)(寵物外出包)</a> <br>
+                    <span class="price"> $1,090</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 寵物零食熱銷榜 -->
+        <div id="Candy" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/點心_4.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>毛孩好棒棒 鴨肉口味20克(狗零食)</a> <br>
+                    <span class="price"> $99</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/點心_5.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>貓咪凍乾零食 鱈魚 60克 (貓零食)</a> <br>
+                    <span class="price"> $240</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/點心_6.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>100%新鮮食材手作寵物鮮食零食 起士雞肉條 100 克 (貓狗零食)</a> <br>
+                    <span class="price"> $200</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 保健食品熱銷榜 -->
+        <div id="Health" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/保健4.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>抗焦慮咀嚼錠30錠(貓狗保健用品)</a> <br>
+                    <span class="price"> $589</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/保健5.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>幼貓高鈣營養膏 50克</a> <br>
+                    <span class="price"> $269</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/保健6.jpg
+                        " style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>保健系列 皮毛好美麗 80錠(185克) 改善貓狗毛髮與皮膚問題 豐富深海魚EPA DHA Omega3</a> <br>
+                    <span class="price"> $269</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 美容熱銷榜 -->
+        <div id="Pretty" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/美容_1.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>草本養護 癢癢退散洗毛精 汪汪專用 500ml</a> <br>
+                    <span class="price"> $460</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/美容_2.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>純天然除蚤驅蟲洗毛精 500ML</a> <br>
+                    <span class="price"> $700</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/美容_3.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>世界級東方草本精油 貓狗適用 沐浴系列 400ml </a> <br>
+                    <span class="price"> $640</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- 住宿熱銷榜 -->
+        <div id="Stay" class="tabcontent">
+            <ul class="hot-foods">
+                <li class="foods" style="height: 200px;">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/住宿_1.jpg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>踏踏貓旅館．貓咪美容</a> <br>
+                    <span class="price">臺中大雅</span>
+                    <button style="width: 80px; bottom: 8px; color: var(--thrcolor);"> <a
+                            href="https://tatacathotel.com/" target="_blank">瞭解更多..</a> </button>
+                </li>
+
+                <li class="foods">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/住宿_2.jpeg" style="width: 180px;"></a>
+                    <a href="#" class="textname"><br>毛小愛Fluv</a> <br>
+                    <span class="price"> 台北大安</span>
+                    <button style="width: 80px; bottom: 8px; color: var(--thrcolor);"> <a href="https://fluv.com/tw"
+                            target="_blank">瞭解更多..</a> </button>
+                </li>
+
+                <li class="foods" style="height: 200px;">
+                    <div class="rank">HOT</div>
+                    <a href="#"><img src="img/住宿_3.jpg" style="width: 180px; height: 130px;"></a>
+
+                    <a href="#" class="textname"><br>阿默寵物美容 </a> <br>
+                    <span class="price"> 新北三峽</span>
+                    <button style="width: 80px; bottom: 8px; color: var(--thrcolor);"> <a
+                            href="http://www.amopetgrooming.com" target="_blank">瞭解更多..</a> </button>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+
+    <br>
+
+    <!-- 商品列表 -->
+    <div class="list" style="text-align: center;">
+        <span>商品列表</span>
+        <div>
+            <ul class="cards">
+                <li class="card">
+                    <a href="#" id="qwert"><img src="img/catfood_1.jpg" style="width: 150px;"></a>
+                    <a href="#" class="textname">Mobby Choice 莫比自然食 低卡化毛配方 (1.5kg)
+                    </a> <br>
+                    <span class="price"> $359</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+                <li class="card">
+                    <a href="#" id="qwert"><img src="img/catfood_2.jpg" style="width: 150px;"></a>
+                    <a href="#" class="textname">Orijen 極緻無穀成幼貓 野牧鮮雞 5.45kg</a> <br>
+                    <span class="price"> $2,299</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+                <li class="card">
+                    <a href="#" id="qwert"><img src="img/catfood_3.jpg" style="width: 150px;"></a>
+                    <a href="#" class="textname">Nutrience 紐崔斯無穀養生貓飼料-火雞肉+雞肉+鯡魚 5kg</a> <br>
+                    <span class="price"> $1,850</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+                <li class="card">
+                    <a href="#" id="qwert"><img src="img/catfood_4.jpg" style="width: 150px;"></a>
+                    <a href="#" class="textname">ND 天然無穀糧 鯡魚甜橙 1.5公斤(貓飼料)</a> <br>
+                    <span class="price"> $1,197</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+                <li class="card">
+                    <a href="#" id="qwert"><img src="img/catfood_1.jpg" style="width: 150px;"></a>
+                    <a href="#" class="textname">經典系列成功貓 化毛高纖1.5KG(貓飼料)</a> <br>
+                    <span class="price"> $408</span>
+                    <button><img src="icons/shopping.png" style="width: 30px;"></button>
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+
+
+    <script type="text/javascript" src="mycontent"></script> 
+    
+</body>
+</html>
---
+++ PHP/style.php
@@ -0,0 +1,483 @@
+<?php
+    header('Content-type: text/css; charset:UTF-8')
+?>
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap");
+
+html {
+    --maincolor: #fff389;
+    --twocolor: #A6372D;
+    --thrcolor: #4D5D73;
+    /* margin: 20px 0; */
+    font-family: 'Noto Sans TC', sans-serif;
+}
+
+/* 這裡是headerCSS設定 */
+.header {
+    position: fixed;
+    background-color: var(--maincolor);
+    overflow: hidden;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1030;
+}
+
+body {
+    margin: 50px 0 0 0;
+}
+
+.header a {
+    float: left;
+    color: var(--thrcolor);
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+.header a:hover {
+    text-shadow: 0 0 10px #FFFFFF, 0 0 15px #FFFFFF, 0 0 20px #FFFFFF;
+}
+
+.logo-centered a {
+    float: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+}
+
+.header-right {
+    float: right;
+}
+
+@media screen and (max-width: 600px) {
+
+    .hearder a,
+    .hearder-right {
+        float: none;
+        display: block;
+    }
+}
+
+/* 這裡是MenuCss設定 */
+.menu {
+    height: 400px;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 10;
+    left: 1;
+    background-color: var(--maincolor);
+    overflow-x: hidden;
+    padding-top: 60px;
+    transition: 0.5s;
+}
+
+.menu a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 17px;
+    /* font-family: 'Noto Sans TC', sans-serif; */
+    color: var(--twocolor);
+    display: block;
+    transition: 0.3s;
+}
+
+/* 經過menu裡面的字變色 */
+.menu a:hover {
+    text-shadow: 0 0 10px #FFFFFF, 0 0 15px #FFFFFF, 0 0 20px #FFFFFF;
+}
+
+/*menu裡面字的設定 */
+.menu .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+
+}
+
+/* menu三條線的設定 */
+.openbtn {
+    font-size: 20px;
+    cursor: pointer;
+    background-color: var(--maincolor);
+    color: var(--twocolor);
+    padding: 10px 15px;
+    border: none;
+}
+
+.openbtn:hover {
+    text-shadow: 0 0 10px #FFFFFF, 0 0 15px #FFFFFF, 0 0 20px #FFFFFF,
+        0 0 25px #FFFFFF;
+}
+
+/* 這裡是中間的icons列 */
+#icons {
+    display: flex;
+    /* align-items: center; */
+    justify-content: center;
+    /* background-color: lightcyan; */
+}
+
+#icons img {
+    width: 45px;
+}
+
+#icons>a {
+    color: 1f317f;
+    margin: 10px;
+    text-align: center;
+    color: gray;
+    text-decoration: none;
+    float: left;
+}
+
+body a:visited {
+    color: gray;
+}
+
+#icons>a:hover {
+    color: var(--twocolor);
+
+}
+
+/* 這裡是搜尋列的CSS設定 */
+
+.search input {
+    width: 250px;
+    border-radius: 30px;
+    border: 2px solid #324B4E;
+    background: #F9F0DA;
+    transition: .3s linear;
+    float: right;
+}
+
+.search input:focus {
+    width: 300px;
+}
+
+.search button {
+    background: none;
+    top: -2px;
+    right: 20px;
+    height: 42px;
+    width: 30px;
+    cursor: pointer;
+    position: absolute;
+}
+
+.search {
+    width: 500px;
+    height: 42px;
+    margin: 0 auto;
+    padding: 5px 0;
+
+}
+
+.search>form {
+    position: relative;
+    width: 300px;
+    margin: 0 auto;
+
+}
+
+input,
+button {
+    border: none;
+    outline: none;
+}
+
+input {
+    width: 100%;
+    height: 30px;
+    padding-left: 13px;
+}
+
+/* 這裡是廣告圖 */
+.slideshow-container {
+    max-width: 900px;
+    position: relative;
+    margin: auto;
+}
+
+/* 隱藏圖片 */
+.mySlides {
+    display: none;
+}
+
+/* 下一頁< 上一頁> */
+.prev,
+.next {
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    width: auto;
+    margin-top: -22px;
+    padding: 16px;
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+    transition: 0.6s ease;
+    border-radius: 0 3px 3px 0;
+    user-select: none;
+}
+
+/* 下一頁框框 */
+.next {
+    right: 0;
+    border-radius: 3px 0 0 3px;
+}
+
+.prev:hover,
+.next:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* 廣告圖下面的點點 */
+.dot {
+    cursor: pointer;
+    height: 10px;
+    width: 10px;
+    margin: 0 2px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+}
+
+
+.dot:hover {
+    background-color: #717171;
+}
+
+.dots {
+    position: absolute;
+    bottom: 5px;
+    right: 45%;
+}
+
+/* 動畫 */
+.fade {
+    animation-name: fade;
+    animation-duration: 1.5s;
+}
+
+@keyframes fade {
+    from {
+        opacity: .4
+    }
+
+    /* opacity元素的不透明度*/
+
+    to {
+        opacity: 1
+    }
+}
+
+/* 熱銷榜 */
+
+.HotList {
+    height: 150px;
+    width: 900px;
+    margin: auto;
+    border: 10px;
+}
+
+/* 熱銷按鈕連結 */
+.tablink {
+    background-color: rgba(255, 254, 254, 0.5);
+    color: var(--thrcolor);
+    float: right;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    font-size: 17px;
+    width: 100px;
+    /* box-shadow: 0 4px 8px 0 rgba(17, 8, 8, 0.3); */
+}
+
+.Hot {
+    color: black;
+    text-shadow: 0 0 15px var(--maincolor), 0 0 10px var(--maincolor);
+    float: left;
+    font-size: 28px;
+    padding: 10px 5px 10px 20px;
+    animation-iteration-count: infinite;
+
+
+}
+
+.tablink:hover {
+    text-shadow: 0 0 15px rgb(194, 60, 60), 0 0 10px rgb(194, 60, 60);
+
+}
+
+/* 熱銷商品裡的商品列 */
+.tabcontent {
+    /* color: white; */
+    display: none;
+    padding: 20px;
+    height: 280px;
+}
+
+/* 熱銷商品設定 */
+.HotList {
+    border: 5px double #1C6EA4;
+    border-radius: 10px;
+    width: 900px;
+    height: 320px;
+}
+
+.hot-foods {
+    display: flex;
+    margin: auto;
+    width: 800px;
+
+}
+
+.foods {
+    box-shadow: 0 4px 4px 0 rgba(17, 8, 8, 0.2);
+    width: 200px;
+    height: 250px;
+    text-align: center;
+    font-family: arial;
+    font-size: small;
+    position: relative;
+    margin: auto;
+
+
+}
+
+.foods button {
+    border: none;
+    outline: 0;
+    background-color: transparent;
+    cursor: pointer;
+    width: 50px;
+    position: absolute;
+    bottom: 0;
+    right: 5px;
+}
+
+.foods button:hover {
+    opacity: 0.7;
+
+}
+
+.foods a {
+    text-decoration: none;
+}
+
+.foods a:hover {
+    color: var(--twocolor);
+}
+
+ol,
+ul {
+    list-style: none;
+}
+
+.rank {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border: 2px;
+    border-color: red;
+    background: #f3f366;
+    clip-path: polygon(0% 0%, 100% 0, 100% 70%, 50% 100%, 0 70%);
+    font-size: 18px;
+    font-family: Arial;
+    text-align: center;
+    line-height: 50px;
+    color: red;
+
+}
+
+/* 商品列表 */
+.list {
+    width: 900px;
+    /* box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2); */
+    margin: auto;
+    /* display: flex; */
+
+    /* border: 5px double #1C6EA4; */
+    border-radius: 10px;
+
+
+}
+
+.list>span {
+    font-size: 20px;
+}
+
+.cards {
+    display: flex;
+
+}
+
+.card {
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2);
+    width: 180px;
+    height: 230px;
+    text-align: center;
+    font-family: arial;
+    font-size: small;
+    position: relative;
+    margin: 10px;
+    right: 20px;
+    padding: 0 5px;
+
+}
+
+/* 熱銷商品等的價錢 */
+.price {
+    color: rgba(255, 0, 0, 0.5);
+    font-size: 18px;
+    position: absolute;
+    bottom: 5px;
+    left: 10px;
+}
+
+.card button {
+    border: none;
+    outline: 0;
+    background-color: transparent;
+    cursor: pointer;
+    width: 50px;
+    position: absolute;
+    bottom: 0;
+    right: 5px;
+}
+
+.card button:hover {
+    opacity: 0.7;
+
+}
+
+.card a {
+    text-decoration: none;
+}
+
+.card a:hover {
+    color: red;
+}
+
+ol,
+ul {
+    list-style: none;
+}
+
+/* 滑鼠移過商品圖片變大 */
+#qwert img {
+    transform: scale(1, 1);
+    transition: all 1s ease-out;
+}
+
+#qwert img:hover {
+    transform: scale(1.2, 1.2);
+}
